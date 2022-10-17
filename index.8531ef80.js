d91d3eb5c05aa5fd07e73f09df097fc105bad1ef
Koverin <110396064+Koverin@users.noreply.github.com>
2022-10-17 18:02:49 +0000

    Deploying to gh-pages from @ Koverin/crazy-team-project@2b7e6159ae10473d39a021bdf264c06723305446 🚀

---
+++ index.8531ef80.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{pagination:{el:".swiper-pagination",clickable:!0,dinamicBullets:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},scrollbar:{el:".swiper-scrollbar"},grabCursor:!0,keyboard:{enabled:!0,onlyInViewport:!0,pageUpDown:!0},slidesPerGroup:1,centeredSlides:!0,loop:!0,autoplay:{delay:3e3}});
+//# sourceMappingURL=index.8531ef80.js.map
