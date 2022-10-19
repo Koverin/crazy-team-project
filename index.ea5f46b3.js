cb6458a1f3e944ce2d207285fbd6bb6671965ae1
Koverin <110396064+Koverin@users.noreply.github.com>
2022-10-19 19:36:00 +0000

    Deploying to gh-pages from @ Koverin/crazy-team-project@5618410c50760300ca2403445378fa71173a536c 🚀

---
+++ index.ea5f46b3.js
@@ -0,0 +1,2 @@
+!function(){var e={openMenuBtn:document.querySelector("[data-menu-open]"),closeMenuBtn:document.querySelector("[data-menu-close]"),menu:document.querySelector("[data-menu]")};function n(){e.menu.classList.toggle("is-open")}document.querySelectorAll(".mobile-menu__menu-link").forEach((function(e){return e.addEventListener("click",n)})),e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n)}();
+//# sourceMappingURL=index.ea5f46b3.js.map
